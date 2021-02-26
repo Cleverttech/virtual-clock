34400cd0d0612d0ae8b2d221f39794c565009be1
Cleverttech <dzifatedeku@gmail.com>
2021-02-26 12:46:20 +0100

    create dynamic mins inside function

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const log= console.log;
-const secondHand = document.querySelector('.second-hand')
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
 
 function setDate(){
   const now = new Date();
   const seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360 ) + 90; //
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  log(seconds);
+ 
+  const mins = now.getMinutes();
+  const minsDegrees = ((mins / 60) * 360 ) + 90;
+  minsHand.style.transform = `rotate(${secondsDegrees}deg)`;
 }
 setInterval(setDate, 1000); // to get the function to run every second
