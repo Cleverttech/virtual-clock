f93f37e7399c4a799b7e54b0c09d311537c8b923
Cleverttech <dzifatedeku@gmail.com>
2021-02-26 12:39:46 +0100

    add secondhand function to transform

---
+++ script.js
@@ -0,0 +1,11 @@
+const log= console.log;
+const secondHand = document.querySelector('.second-hand')
+
+function setDate(){
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360 ) + 90; //
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  log(seconds);
+}
+setInterval(setDate, 1000); // to get the function to run every second
